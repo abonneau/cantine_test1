e93688b5162ee3970b1a9f7ac82bd08f1875db94
abonneau <aurelienbonneau@free.fr>
2012-09-23 13:09:27 +0200

    arret des test

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,9 +4,10 @@
 require 'spec_helper'
 
 describe HomeController do
+	render_views
 
   describe "GET 'index'" do
-    it "returns http success" do
+    it "devrait réussir" do
       get 'index'
       response.should be_success
     end
