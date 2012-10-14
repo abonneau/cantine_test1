ba9f79d6908dd3062fd72d44c6e8f3693bca13b6
abonneau <aurelienbonneau@free.fr>
2012-10-14 14:15:11 +0200

    gestion des utilisateur par cancan et rolify

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
 	def users
 		@users = User.all
 	end
+	def affichage_bouttons?
+		current_user.has_role? :admin or current_user == @recette.user
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120927162910) do
+ActiveRecord::Schema.define(:version => 20121014091511) do
 
   create_table "recettes", :force => true do |t|
     t.string   "titre"
-    t.text     "description",
-    t.text     "ingredients",
-    t.text     "preparation",
+    t.text     "description", :limit => 255
+    t.text     "ingredients", :limit => 255
+    t.text     "preparation", :limit => 255
     t.integer  "user_id"
     t.datetime "created_at",                 :null => false
     t.datetime "updated_at",                 :null => false
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(:version => 20120927162910) do
 
   add_index "recettes", ["titre"], :name => "index_recettes_on_titre"
   add_index "recettes", ["user_id"], :name => "index_recettes_on_user_id"
+
+  create_table "roles", :force => true do |t|
+    t.string   "name"
+    t.integer  "resource_id"
+    t.string   "resource_type"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
+  add_index "roles", ["name", "resource_type", "resource_id"], :name => "index_roles_on_name_and_resource_type_and_resource_id"
+  add_index "roles", ["name"], :name => "index_roles_on_name"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -44,5 +55,12 @@ ActiveRecord::Schema.define(:version => 20120927162910) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "users_roles", :id => false, :force => true do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+  end
+
+  add_index "users_roles", ["user_id", "role_id"], :name => "index_users_roles_on_user_id_and_role_id"
 
 end
