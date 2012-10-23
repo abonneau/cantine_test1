af18bfd3ec3541b7107e5a472dc566a35e1115ed
abonneau <aurelienbonneau@free.fr>
2012-10-23 19:31:21 +0200

    ajout d'une pagination

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
 	#load_and_authorize_resource
   def index
 	  @users = User.all
-	  @recettes = Recette.all
+	  @recettes = Recette.paginate(:page => params[:page], :per_page => 5)
 	end
 end
